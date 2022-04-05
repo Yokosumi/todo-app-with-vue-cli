35a861eb01b959a58a8e7551685906b5164343cd
unknown <selman.ciftci@outlook.com>
2022-04-05 11:16:16 +0200

    feature: added basis of a todo app

---
+++ script.js
@@ -0,0 +1,39 @@
+// todo button
+// array mit den todos
+// todo list <ul>
+// funktion um todos hinzufügen
+// input field
+
+const app = Vue.createApp({
+  data() {
+    return {
+      list: [
+        {
+          todo: "Learn HTML",
+          done: false,
+        },
+        {
+          todo: "Learn CSS",
+          done: false,
+        },
+        {
+          todo: "Learn Vue",
+          done: false,
+        },
+      ],
+      newTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.list.push({
+        todo: this.newTodo,
+        done: false,
+      }),
+        (this.newTodo = "");
+    },
+    deleteTodo(index) {
+      this.list.splice(index, 1);
+    },
+  },
+}).mount("#app");
